42c0d91490746cc94d47c39f3efe70bfc35c97d4
muhammedsouban <muhammedsoubanbi@gmail.com>
2023-05-17 10:48:45 +0530

    Updated server

--- Client/src/components/user/Hero/hero.jsx
+++ Client/src/components/user/Hero/hero.jsx
@@ -8,14 +8,11 @@ function Hero() {
             <div className='hero'>
                 <img src="http://localhost:8080/public/images/1681886137730-Me.JPG"
                     alt="" />
-
                 <div className="hero-text text-start">
                     <h1 >Bringing skilled Professionals To You</h1>
                     <input type="text" placeholder='Search For Service' />
-
                 </div>
             </div>
-
         </>
     )
 }
--- Server/app.js
+++ Server/app.js
@@ -1,38 +1,46 @@
-import express from "express";
-import cors from 'cors'
-import morgan from "morgan";
-import cookieParser from "cookie-parser";
-import mongoose from "mongoose";
-import adminRoute from "./routes/adminRoute.js";
-import userRoute from "./routes/userRoute.js";
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import adminRoute from './routes/adminRoute.js';
+import userRoute from './routes/userRoute.js';
 
-mongoose.connect('mongodb://127.0.0.1:27017/myapp', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB!');
-}).catch((err) => {
-  console.log('Failed to connect to MongoDB:', err);
-});
+dotenv.config();
+
+const { MONGODB_URI, PORT } = process.env;
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log('Failed to connect to MongoDB:', err);
+  });
+
 mongoose.set('strictQuery', true);
 
+const app = express();
 
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(morgan('tiny'))
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/public/images',express.static('public/images'));
-
-const port = 8080
+app.use('/public/images', express.static('public/images'));
 
 app.get('/', (req, res) => {
-    res.status(201).json("Home GET Request")
-})
-app.use('/',userRoute)
-app.use('/admin',adminRoute)
+  res.status(201).json('Home GET Request');
+});
 
-app.listen(port, () => [
-    console.log(`server connected to http://localhost:${port}`)
-])
+app.use('/', userRoute);
+app.use('/admin', adminRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server connected to http://localhost:${PORT}`);
+});
